62899564ae0d7127d915465740744cece3089f2a
coder <sergeinerobeev@gmail.com>
2024-11-05 19:37:21 +0100

    init project

---
+++ src/main/java/hu/nero/worldWeather/WwApplication.java
@@ -0,0 +1,13 @@
+package hu.nero.worldWeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/nero/worldWeather/WwApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nero.worldWeather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
