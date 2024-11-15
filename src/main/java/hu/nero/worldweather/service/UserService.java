492fbfdb01545e2571e5cda52f36bc5c42923684
Sergei Nerobeev <sergeinerobeev@gmail.com>
2024-11-15 09:13:15 +0100

    yellow label getAllUsers

--- src/main/java/hu/nero/worldweather/service/UserService.java
+++ src/main/java/hu/nero/worldweather/service/UserService.java
@@ -18,26 +18,27 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public List<User> getAllUsers() { //todo all
-    return List.of(
-        new User(
-            1L,
-            "Joe",
-            "joeb@mail.com",
-            LocalDate.of(1940, 12, 3)
-        ),
-        new User(
-            2L,
-            "Donald",
-            "donaldt@gmail.com",
-            LocalDate.of(1945, 8, 12)
-        ),
-        new User(
-            3L,
-            "Barak",
-            "barakko@yahoo.com",
-            LocalDate.of(1955, 3, 23)
-        )
-    );
+    public List<User> getAllUsers() {
+//    return List.of(
+//        new User(
+//            1L,
+//            "Joe",
+//            "joeb@mail.com",
+//            LocalDate.of(1940, 12, 3)
+//        ),
+//        new User(
+//            2L,
+//            "Donald",
+//            "donaldt@gmail.com",
+//            LocalDate.of(1945, 8, 12)
+//        ),
+//        new User(
+//            3L,
+//            "Barak",
+//            "barakko@yahoo.com",
+//            LocalDate.of(1955, 3, 23)
+//        )
+//    );
+        return userRepository.findAll();
   }
 }
